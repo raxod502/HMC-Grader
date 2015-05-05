11a93f3a434d3e5502565a842695790bf27b8362
Jeb Brooks <robojeb@gmail.com>
2015-05-05 16:31:51 -0700

    Began documentation book and added temp value warnings to the config file

--- config.py
+++ config.py
@@ -6,23 +6,26 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #
 # Settings for WTForms
 #
+# Current settings are for testing only change for production
 CSRF_ENABLES=True
 SECRET_KEY="Grutors <3 SPAM"
 
 #
 #Mongo settings
 #
+# Current settings are for testing only change for production
 MONGODB_SETTINGS = {'DB': 'submissionsite',
 'username': 'grader',
 'password': 'grutorsLoveGrading',
-'host': '134.173.43.77'}
+'host': '134.173.43.46'}
 
 DATABASE_QUERY_TIMEOUT = 0.5
 
 #
 #Celery config ssettings
 #
-CELERY_BROKER_URL="amqp://grader:grutorsLoveGrading@134.173.43.77"
+# Current settings are for testing only change for production
+CELERY_BROKER_URL="amqp://grader:grutorsLoveGrading@134.173.43.46"
 FLOWER_ACCESS_URL="http://127.0.0.1:5555"
 
 #

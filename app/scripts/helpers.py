1feee266f5e4b00129bbcc43dd8b63908766c156
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-07-13 13:22:31 -0700

    Fixed print statements and added alerts when adding user as grutor or student
    Print statements for when the course was not found were wrong previously
    New print statements included to alert user when courses are being added
    query_yes_no() function added to helpers.py to ensure courses are correct
    addGrutorToMultipleCourses.py and addStudentToMultipleCourses.py both updated

--- app/scripts/helpers.py
+++ app/scripts/helpers.py
@@ -1,6 +1,37 @@
 from app.structures.models.user import *
 from app.structures.models.course import *
 
+def query_yes_no(question, default="yes"):
+  """Ask a yes/no question via raw_input() and return their answer.
+
+  "question" is a string that is presented to the user.
+  "default" is the presumed answer if the user just hits <Enter>.
+      It must be "yes" (the default), "no" or None (meaning
+      an answer is required of the user).
+
+  The "answer" return value is True for "yes" or False for "no".
+  """
+  valid = {"yes": True, "y": True, "ye": True,
+           "no": False, "n": False}
+  if default is None:
+    prompt = " [y/n] "
+  elif default == "yes":
+    prompt = " [Y/n] "
+  elif default == "no":
+    prompt = " [y/N] "
+  else:
+    raise ValueError("invalid default answer: '%s'" % default)
+
+  while True:
+    print question + prompt
+    choice = raw_input().lower()
+    if default is not None and choice == '':
+      return valid[default]
+    elif choice in valid:
+      return valid[choice]
+    else:
+      print "Please respond with 'yes' or 'no' (or 'y' or 'n').\n"
+
 def createUsername(firstName, lastName):
   firstName = firstName.lower()
   lastName = lastName.lower()

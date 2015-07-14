09ccd39287eded84012da1fa939bc206b0e5c0c5
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-07-14 15:56:11 -0700

    Added getSubmission.py
    Note: this version has not been tested because the files are not
    on my local machine and cannot be accessed. Not sure if it works
    with the knuth file system either.
    This script should ideally allow the user to get the actual student
    submission files by specifying the assignment and problem.

---
+++ app/scripts/getSubmission.py
@@ -0,0 +1,42 @@
+import sys
+
+from app.scripts.helpers import *
+from app.structures.models.user import *
+from app.structures.models.course import *
+from app.structures.models.gradebook import *
+
+if __name__ == "__main__":
+  username = raw_input("Username: ")
+  
+  user = getUser(username)
+
+  if user == None:
+    print "Could not find the user you specified"
+    sys.exit(1)
+
+  for i, c in enumerate(user.courseStudent):
+    print "%d) %s" % (i, c.name)
+
+  index = int(raw_input("Pick a course: "))
+
+  course = user.courseStudent[index]
+
+  for i, a in enumerate(course.assignments):
+    print "%d) %s" % (i, a.name)
+
+  index = int(raw_input("Pick an assignment: "))
+
+  assignment = course.assignments[index]
+
+  for i, p in enumerate(assignment.problems):
+    print "%d) %s" % (i, p.name)
+
+  index = int(raw_input("Pick a problem: "))
+
+  problem = assignment.problems[index]
+
+  subNum = problem.getSubmissionNumber(user)
+
+  sub = problem.getFiles(user, subNum)
+
+  return sub

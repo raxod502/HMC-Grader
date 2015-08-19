2d5f29e6f7d3076d0ddf66877ce46e432f113a45
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-08-19 12:25:59 -0700

    Locks to database fixed, backups now uploaded to Amazon S3

--- mongo_backup.sh
+++ mongo_backup.sh
@@ -13,13 +13,13 @@ TIMESTAMP=`date +%F-%H%M`
 BACKUPS_DIR="/home/hmcgrader/mongodb-backups"
 BACKUP_NAME="$SERVER_NAME-$TIMESTAMP"
 
-S3_BUCKET_NAME="bucketname_here" #replace with your bucket name on Amazon S3
+S3_BUCKET_NAME="submissionstorage" #replace with your bucket name on Amazon S3
 S3_BUCKET_PATH="mongodb-backups"
 
 # Create backup, with locks to force file syncronization and lock writes
-mongo admin --eval "printjson(db.fsyncLock())"
+mongo $MONGO_DATABASE -u $ADMIN_USER -p $ADMIN_PASSWORD --eval "db.fsyncLock()"
 $MONGODUMP_PATH --host $MONGO_HOST:$MONGO_PORT --username $ADMIN_USER --password $ADMIN_PASSWORD --db $MONGO_DATABASE
-mongo admin --eval "printjson(db.fsyncUnlock())"
+mongo $MONGO_DATABASE -u $ADMIN_USER -p $ADMIN_PASSWORD --eval "db.fsyncUnlock()"
 
 mkdir -p $BACKUPS_DIR
 mv dump $BACKUP_NAME
@@ -27,5 +27,5 @@ tar -zcvf $BACKUPS_DIR/$BACKUP_NAME.tgz $BACKUP_NAME
 rm -rf $BACKUP_NAME
 
 # Upload to S3
-# s3cmd put $BACKUP_NAME.tgz
-#    s3://$S3_BUCKET_NAME/$S3_BUCKET_PATH/$BACKUP_NAME.tgz
+s3cmd put $BACKUPS_DIR/$BACKUP_NAME.tgz s3://$S3_BUCKET_NAME/$S3_BUCKET_PATH/$BACKUP_NAME.tgz
+

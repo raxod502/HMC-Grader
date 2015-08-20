41c618b2459d6687cbdbce42d5a46761f2827636
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-08-20 13:01:59 -0700

    Refining addUsersFromList.py to work with sample class lists
    * Changes made after running the script with files from Dodds which have more
      information than is needed to create users
    * CSV file has to be read into the script in "universal" mode
    * Added user inputs. Now the user can:
        * Adjust how many lines to ignore at the top of the file
        * Specify the indices for the name and email columns
    * Added a block to print one user entry before actually any users. This is
      meant to safeguard against the case that the column indices are not correct
      and give the user the chance to abort
    * Added email to printed output
    * Still need to make the script output a file in addition to the printed info

--- app/scripts/addUsersFromList.py
+++ app/scripts/addUsersFromList.py
@@ -33,28 +33,60 @@ Users should be added as:
     print "Not a valid user type"
     sys.exit(1)
 
+  numRowsToClear = int(raw_input("Number of rows to ignore at the top of the csv: "))
+  nameIndex = int(raw_input("Index of name column: "))
+  emailIndex = int(raw_input("Index of email column: "))
+
   try:
     #Actually read the file to get the users info
-    with open(sys.argv[1], 'r') as csvFile:
+    with open(sys.argv[1], 'rU') as csvFile:
       studentReader = csv.reader(csvFile, delimiter=',', quotechar='"')
+
+      #clear rows that do not have student info
+      for i in range(numRowsToClear):
+        studentReader.next()
+    
+      #special case for the first loop to check column numbers
+      firstLoop = True
 
       #Read info from CSV file
       for row in studentReader:
-        lastName = row[0]
-        firstName = row[1]
-        email = row[2].strip()
+        name = row[nameIndex]
+        email = row[emailIndex].strip()
 
-        firstNameClean = cleanName(firstName)
+        lastName, firstMidName = name.split(",")
+        lastName = lastName.strip()
+        firstMidName = firstMidName.strip()
+
+        firstMidNameClean = cleanName(firstMidName)
         lastNameClean = cleanName(lastName)
 
-        u = addOrGetUser(firstNameClean, lastNameClean, email)
+        if firstLoop:
+          firstLoop = False
+          usernameEx = createUsername(firstMidNameClean, lastNameClean)
+          print "Your settings would result in entries like:"
+          print lastName + ", "+ firstMidName + " " + "(" + usernameEx + "): " + email
+
+          continueScript = query_yes_no("Do you want to use these index numbers?")
+
+          if not continueScript:
+            print "Please rerun this script with the correct column indexes"
+            sys.exit(1)
+
+          else:
+            if userType == 0:
+              print "Adding users as students to " + course.name + " for " + course.semester
+            if userType == 1:
+              print "Adding users as grutors to " + course.name + " for " + course.semester
+
+        u = addOrGetUser(firstMidNameClean, lastNameClean, email)
         if userType == 0:
           u.courseStudent.append(course)
         elif userType == 1:
           u.courseGrutor.append(course)
         u.save()
 
-        print lastName + ", "+ firstName + " " + "(" + u.username + ")"
+        print lastName + ", "+ firstMidName + " " + "(" + u.username + "): " + email
   except:
     print "Please provide a csv file"
     print "Usage is:\n <virtual-env>/bin/python " + sys.argv[0]+ " <path-to-csv-file>"

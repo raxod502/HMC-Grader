f437c13bb588ca15affc498db36603a644fcd136
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-09-10 15:35:45 -0700

    CS5GR HW0 firstTests.py: tests changed to reflect new rubric
    Main change is that palindrome is no longer a required function,
    so these tests have been removed

--- TestFiles/CS5GreenTests/hw0tests/firstTests.py
+++ TestFiles/CS5GreenTests/hw0tests/firstTests.py
@@ -5,22 +5,18 @@ import unittest
 class PowerTest(unittest.TestCase):
     def testPowerFour(self):
         self.assertEqual(hw.power(4), 256)
-    def testPowerZero(self):
-        self.assertEqual(hw.power(0), 1)
 
 class StringMultiplyTest(unittest.TestCase):
     def testStringMultiplySpam(self):
         self.assertEqual(hw.stringMultiply("spam",4), "spamspamspamspam")
+    def testStringMultiplyAB(self):
+        self.assertEqual(hw.stringMultiply("ab",2), "abab")
 
 class ListMakerTest(unittest.TestCase):
     def testListMakerMaps(self):
         self.assertEqual(hw.listMaker("maps",7), ['maps', 'maps', 'maps', 'maps', 'maps', 'maps', 'maps'])
-
-class PalindromeMakerTest(unittest.TestCase):
-    def testPalindromeShort(self):
-        self.assertEqual(hw.palindromeMaker('ab'), 'abba')
-    def testPalindromeLong(self):
-        self.assertEqual(hw.palindromeMaker('a man a plan a canal panama'), 'a man a plan a canal panamaamanap lanac a nalp a nam a')
+    def testListMakerCharA(self):
+        self.assertEqual(hw.listMaker('a',3), ['a', 'a', 'a'])
 
 if __name__ == '__main__':
     unittest.main()

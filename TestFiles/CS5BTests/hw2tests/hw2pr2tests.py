8874fcc87a7e3879f520dfd89fa36a377d78338e
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-09-21 13:28:19 -0700

    CS5B hw2pr2 tests: more errors in tests fixed

--- TestFiles/CS5BTests/hw2tests/hw2pr2tests.py
+++ TestFiles/CS5BTests/hw2tests/hw2pr2tests.py
@@ -8,7 +8,7 @@ class fancyLCSTest(unittest.TestCase):
     def testLCSSingleChars(self):
         self.assertEqual(hw.fancyLCS("a", "z"), [0, '#', '#'])
     def testLCSNoCommonSeq(self):
-        self.assertEqual(hw.fancyLCS("cs", "awesome"), [1, '#s', '###s###'])
+        self.assertEqual(hw.fancyLCS("cs", "amazing"), [0, '##', '#######'])
     def testLCSSamePosition(self):
         self.assertEqual(hw.fancyLCS("hi", "high"), [2, 'hi', 'hi##'])
     def testLCSDiffPosition(self):
@@ -18,9 +18,9 @@ class alignTest(unittest.TestCase):
     def testAlignEmptyString(self):
         self.assertEqual(hw.align("", "spam"), [0, '----', 'spam'])
     def testAlignSingleChars(self):
-        self.assertTrue((hw.align("a", "z") == [0, 'a-', '-z']) or (hw.fancyLCS("a", "z") == [0, '-a', 'z-']))
+        self.assertTrue((hw.align("a", "z") == [0, 'a-', '-z']) or (hw.align("a", "z") == [0, '-a', 'z-']))
     def testAlignNoCommonSeq(self):
-        self.assertEqual(hw.align("cs", "awesome"), [0, 'cs-------', '--awesome'])
+        self.assertTrue((hw.align("cs", "amazing") == [0, 'cs-------', '--amazing']) or (hw.align("cs", "amazing") == [0, '-------cs', 'amazing--']))
     def testAlignSamePosition(self):
         self.assertEqual(hw.align("hi", "high"), [2, 'hi--', 'high'])
     def testAlignDNA(self):

72c331ce92d2b1c49e948b92565e81171762b0ed
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-09-21 20:48:19 -0700

    CS60 HW2: Added test files for Fall 2015

---
+++ TestFiles/CS60Tests/hw2/removealltests.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(include "hw2pr1_remove-all.rkt")
+(require rackunit)
+(require (prefix-in rackUnit: rackunit/text-ui))
+
+(define file-tests
+  (test-suite
+   "tests"
+    (check-equal? (remove-all "i" '("a" "l" "i" "i" "i" "e" "n")) '("a" "l" "e" "n")
+                  "remove-all middle")
+    (check-equal? (remove-all "i" '( ("a" "l" "i") "i" "i" "e" "n")) '(("a" "l" "i") "e" "n")
+                  "remove-all sublist: Don't go into sublists")
+    (check-equal? (remove-all 0 '(1 0 1 0 1 0))  '(1 1 1)
+                  "remove-all given")
+    (check-equal? (remove-all 0 '())  '()
+                  "remove-all from empty")
+  )
+)
+
+(rackUnit:run-tests file-tests 'normal)

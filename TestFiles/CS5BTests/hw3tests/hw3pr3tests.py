0582529100269a1293f9b1b7a5e5746f12c49687
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-10-01 10:27:40 -0700

    CS5B hw3pr3 getFold: tests fixed to use isMatchingRNAPair helper
    Before, the tests for the extra credit getFold() function were incorrect
    because they checked if the pairs of nucleotides were literally the same
    character. Now, there is a helper function to check if the pairs are valid
    rna pairings (A/U or C/G).

--- TestFiles/CS5BTests/hw3tests/hw3pr3tests.py
+++ TestFiles/CS5BTests/hw3tests/hw3pr3tests.py
@@ -2,6 +2,19 @@ import hw3pr3 as hw
 
 import unittest
 
+def isMatchingRNAPair(char1, char2):
+    """
+        a function to help test getFold
+        Takes two characters (the two RNA nucleotides)
+        Outputs true if they are an A/U pair or a C/G pair
+    """
+    if (char1 == "A" and char2 == "U") or (char1 == "U" and char2 == "A"): 
+        return True 
+    elif (char1 == "C" and char2 == "G") or (char1 == "G" and char2 == "C"):
+        return True
+    else:
+        return False
+
 class FoldTest(unittest.TestCase):
     def testFold1(self):
         self.assertEqual(hw.fold("ACCCCCU"), 1)
@@ -33,40 +46,40 @@ class GetFoldTest(unittest.TestCase):
         testString = "ACCCCCU"
         result = hw.getFold(testString)
         self.assertEqual(result[0], 1)
-        self.assertEqual(testString[result[1][0][0]], testString[result[1][0][1]])
+        self.assertTrue(isMatchingRNAPair(testString[result[1][0][0]], testString[result[1][0][1]]))
     def testGetFold2(self):
         testString = "ACCCCGU"
         result = hw.getFold(testString)
         self.assertEqual(result[0], 2)
-        self.assertEqual(testString[result[1][0][0]], testString[result[1][0][1]])
-        self.assertEqual(testString[result[1][1][0]], testString[result[1][1][1]])
+        self.assertTrue(isMatchingRNAPair(testString[result[1][0][0]], testString[result[1][0][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][1][0]], testString[result[1][1][1]]))
     def testGetFold3(self):
         testString = "AAUUGCGC"
         result = hw.getFold(testString)
         self.assertEqual(result[0], 4)
-        self.assertEqual(testString[result[1][0][0]], testString[result[1][0][1]])
-        self.assertEqual(testString[result[1][1][0]], testString[result[1][1][1]])
-        self.assertEqual(testString[result[1][2][0]], testString[result[1][2][1]])
-        self.assertEqual(testString[result[1][3][0]], testString[result[1][3][1]])
+        self.assertTrue(isMatchingRNAPair(testString[result[1][0][0]], testString[result[1][0][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][1][0]], testString[result[1][1][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][2][0]], testString[result[1][2][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][3][0]], testString[result[1][3][1]]))
     def testGetFold4(self):
         testString = "ACUGAGCCCU"
         result = hw.getFold(testString)
         self.assertEqual(result[0], 3)
-        self.assertEqual(testString[result[1][0][0]], testString[result[1][0][1]])
-        self.assertEqual(testString[result[1][1][0]], testString[result[1][1][1]])
-        self.assertEqual(testString[result[1][2][0]], testString[result[1][2][1]])
+        self.assertTrue(isMatchingRNAPair(testString[result[1][0][0]], testString[result[1][0][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][1][0]], testString[result[1][1][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][2][0]], testString[result[1][2][1]]))
     def testGetFold5(self):
         testString = "ACUGAGCCCUGUUAGCUAA"
         result = hw.getFold(testString)
         self.assertEqual(result[0], 8)
-        self.assertEqual(testString[result[1][0][0]], testString[result[1][0][1]])
-        self.assertEqual(testString[result[1][1][0]], testString[result[1][1][1]])
-        self.assertEqual(testString[result[1][2][0]], testString[result[1][2][1]])
-        self.assertEqual(testString[result[1][3][0]], testString[result[1][3][1]])
-        self.assertEqual(testString[result[1][4][0]], testString[result[1][4][1]])
-        self.assertEqual(testString[result[1][5][0]], testString[result[1][5][1]])
-        self.assertEqual(testString[result[1][6][0]], testString[result[1][6][1]])
-        self.assertEqual(testString[result[1][7][0]], testString[result[1][7][1]])
+        self.assertTrue(isMatchingRNAPair(testString[result[1][0][0]], testString[result[1][0][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][1][0]], testString[result[1][1][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][2][0]], testString[result[1][2][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][3][0]], testString[result[1][3][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][4][0]], testString[result[1][4][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][5][0]], testString[result[1][5][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][6][0]], testString[result[1][6][1]]))
+        self.assertTrue(isMatchingRNAPair(testString[result[1][7][0]], testString[result[1][7][1]]))
 
 
 if __name__ == '__main__':

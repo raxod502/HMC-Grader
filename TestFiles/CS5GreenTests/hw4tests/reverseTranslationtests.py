47ec1f71e01d3744f715b4a3efc72fe64d7ddc61
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-10-05 16:12:27 -0700

    CS5Gr HW4 tests fixed

--- TestFiles/CS5GreenTests/hw4tests/reverseTranslationtests.py
+++ TestFiles/CS5GreenTests/hw4tests/reverseTranslationtests.py
@@ -5,11 +5,11 @@ import unittest
 class ReverseTest(unittest.TestCase):
   def testReverse1(self):
     temp = sorted(hw.reverse({"a":"1","b":"2","c":"3","d":"3"}).items());
-    self.assertTrue((temp == [('1', 'a'), ('2', 'b'), ('3', 'c or d')]) or (temp == [('1', 'a'), ('2', 'b'), ('3', 'd or c')]))
+    self.assertTrue((temp == [('1', 'a'), ('2', 'b'), ('3', 'c or d')]) or (temp == [('1', 'a'), ('2', 'b'), ('3', 'd or c')]), temp)
 
   def testReverse2(self):
     temp2 = sorted(hw.reverse({'dude!' : 'hello', 'later' : 'goodbye', 'stoked' : 'happy', 'ciao' : 'goodbye'}).items()); 
-    self.assertTrue((temp2 == [('goodbye', 'ciao or later'), ('happy', 'stoked'), ('hello', 'dude!')]) or (temp2 == [('goodbye', 'later or ciao'), ('happy', 'stoked'), ('hello', 'dude!')]))
+    self.assertTrue((temp2 == [('goodbye', 'ciao or later'), ('happy', 'stoked'), ('hello', 'dude!')]) or (temp2 == [('goodbye', 'later or ciao'), ('happy', 'stoked'), ('hello', 'dude!')]), temp2)
 
   def testReverse3(self):
     self.assertEqual(sorted(hw.reverse({"*":45,"&":13,"-":9}).items()), [(9, '-'), (13, '&'), (45, '*')])

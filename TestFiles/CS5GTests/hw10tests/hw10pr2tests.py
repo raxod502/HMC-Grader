c04fc930475526a2c387984d38bacc55c7a07f32
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-11-23 15:48:22 -0800

    CS5G: Added tests for hw6, hw9, hw10 and hw11

--- TestFiles/CS5GTests/hw10tests/hw10pr2tests.py
+++ TestFiles/CS5GTests/hw10tests/hw10pr2tests.py
@@ -1,158 +1,131 @@
-#
-# Connect 4 board grading script
-#
+import hw10pr2 as hw
 
+import unittest
 
-from hw10pr2 import *
+class ConnectFourTests(unittest.TestCase):
+  # test 1 - does addMove work in the last column?
+  def testAddMoveLastColumn(self):
+    b = hw.Board(7,6)
+    b.addMove(6,'X')
+    answer1 = [ [' ']*7 for i in range(6) ]
+    answer1[5][6] = 'X'
+    self.assertEqual(b.data, answer1, '** INCORRECT **  X should be in the lower right')
 
-# test 1 - does addMove work in the last column?
-print "\n\nTEST 1"
-b = Board(7,6)
-b.addMove(6,'X')
-print b
-answer1 = [ [' ']*7 for i in range(6) ]
-answer1[5][6] = 'X'
-if b.data == answer1:
-    print 'correct... X should be in the lower right'
-else:
-    print '** INCORRECT **  X should be in the lower right'
+  # test 2 - does delMove work?
+  def testDelMove(self):
+    b = hw.Board(7,6)
+    b.addMove(6,'X')
+    b.delMove(6)
+    answer2 = [ [' ']*7 for i in range(6) ]
+    self.assertEqual(b.data, answer2,'** INCORRECT **  board should be empty')
 
+  # test 3 - does isFull work?
+  def testIsFullTrue(self):
+    b = hw.Board(2,2)
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 1, 'X' )
+    b.addMove( 1, 'X' )
+    self.assertTrue(b.isFull(),'** INCORRECT **  b actually is full')
 
-# test 2 - does delMove work?
-print "\n\nTEST 2"
-b.delMove(6)
-print b
-answer2 = [ [' ']*7 for i in range(6) ]
-if b.data == answer2:
-    print 'correct... board should be empty'
-else:
-    print '** INCORRECT **  board should be empty'
+  # test 4 - another isFull
+  def testIsFullFalse(self):
+    b = hw.Board(2,2)
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 1, 'X' )
+    b.addMove( 1, 'X' )
+    b.delMove(1)
+    self.assertFalse(b.isFull(),'** INCORRECT **  b actually is not full')
 
+  # test 5 - wins for horizontal
+  def testWinsForHorizontalFalse(self):
+    b = hw.Board(7,6)
+    b.addMove( 0, 'X' )
+    b.addMove( 1, 'X' )
+    b.addMove( 2, 'X' )
+    self.assertFalse(b.winsFor('X'),'** INCORRECT **  b does not win for X')
 
+  def testWinsForHorizontalTrue(self):
+    b = hw.Board(7,6)
+    b.addMove( 0, 'X' )
+    b.addMove( 1, 'X' )
+    b.addMove( 2, 'X' )
+    b.addMove( 3, 'X' )
+    self.assertTrue(b.winsFor('X'),'** INCORRECT **  b does win for X')
 
-# test 3 - does isFull work?
-print "\n\nTEST 3"
-b = Board(2,2)
-b.addMove( 0, 'X' )
-b.addMove( 0, 'X' )
-b.addMove( 1, 'X' )
-b.addMove( 1, 'X' )
-print b
-if b.isFull():
-    print 'correct... b is full.'
-else:
-    print '** INCORRECT **  b actually is full'
+  # test 6 - wins for vertical
+  def testWinsForVerticalFalse(self):
+    b = hw.Board(7,6)
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    self.assertFalse(b.winsFor('X'),'** INCORRECT **  b does not win for X')
 
+  def testWinsForVerticalTrue(self):
+    b = hw.Board(7,6)
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    self.assertTrue(b.winsFor('X'),'** INCORRECT **  b does win for X')
 
+    # test 7 - wins for diagonal (SW)
+  def testWinsForDiagonalSWFalse(self):
+    b = hw.Board(7,6)
+    b.addMove( 0, 'X' )
+    b.addMove( 1, 'O' )
+    b.addMove( 1, 'X' )
+    b.addMove( 2, 'O' )
+    b.addMove( 2, 'X' )
+    b.addMove( 2, 'X' )
+    b.addMove( 3, 'X' )
+    b.addMove( 3, 'O' )
+    b.addMove( 3, 'X' )
+    self.assertFalse(b.winsFor('X'),'** INCORRECT **  b does not win for X')
 
+  def testWinsForDiagonalSWTrue(self):
+    b = hw.Board(7,6)
+    b.addMove( 0, 'X' )
+    b.addMove( 1, 'O' )
+    b.addMove( 1, 'X' )
+    b.addMove( 2, 'O' )
+    b.addMove( 2, 'X' )
+    b.addMove( 2, 'X' )
+    b.addMove( 3, 'X' )
+    b.addMove( 3, 'O' )
+    b.addMove( 3, 'X' )
+    b.addMove( 3, 'X' )
+    self.assertTrue(b.winsFor('X'),'** INCORRECT **  b does win for X')
 
-# test 4 - another isFull
-print "\n\nTEST 4"
-b.delMove(1)
-print b
-if b.isFull():
-    print '** INCORRECT **  b actually is not full'
-else:
-    print 'correct... b is not full.'
+  # test 8 - wins for diagonal (SE)
+  def testWinsForDiagonalSEFalse(self):
+    b = hw.Board(7,6)
+    b.addMove( 6, 'X' )
+    b.addMove( 5, 'O' )
+    b.addMove( 5, 'X' )
+    b.addMove( 4, 'O' )
+    b.addMove( 4, 'X' )
+    b.addMove( 4, 'X' )
+    b.addMove( 3, 'X' )
+    b.addMove( 3, 'O' )
+    b.addMove( 3, 'X' )
+    self.assertFalse(b.winsFor('X'),'** INCORRECT **  b does not win for X')
 
+  def testWinsForDiagonalSETrue(self):
+    print "\n\nTEST 8"
+    b = hw.Board(7,6)
+    b.addMove( 6, 'X' )
+    b.addMove( 5, 'O' )
+    b.addMove( 5, 'X' )
+    b.addMove( 4, 'O' )
+    b.addMove( 4, 'X' )
+    b.addMove( 4, 'X' )
+    b.addMove( 3, 'X' )
+    b.addMove( 3, 'O' )
+    b.addMove( 3, 'X' )
+    b.addMove( 3, 'X' )
+    self.assertTrue(b.winsFor('X'),'** INCORRECT **  b does win for X')
 
-
-# test 5 - wins for horizontal
-print "\n\nTEST 5"
-b = Board(7,6)
-b.addMove( 0, 'X' )
-b.addMove( 1, 'X' )
-b.addMove( 2, 'X' )
-print b
-if b.winsFor('X'):
-    print '** INCORRECT **  b does not win for X'
-else:
-    print 'correct... b does not win for X'
-
-
-b.addMove( 3, 'X' )
-print b
-if b.winsFor('X'):
-    print 'correct... b does win for X'
-else:
-    print '** INCORRECT **  b does win for X'
-
-
-
-# test 6 - wins for vertical
-print "\n\nTEST 6"
-b = Board(7,6)
-b.addMove( 0, 'X' )
-b.addMove( 0, 'X' )
-b.addMove( 0, 'X' )
-print b
-if b.winsFor('X'):
-    print '** INCORRECT **  b does not win for X'
-else:
-    print 'correct... b does not win for X'
-
-
-b.addMove( 0, 'X' )
-print b
-if b.winsFor('X'):
-    print 'correct... b does win for X'
-else:
-    print '** INCORRECT **  b does win for X'
-
-
-
-# test 7 - wins for diagonal (SW)
-print "\n\nTEST 7"
-b = Board(7,6)
-b.addMove( 0, 'X' )
-b.addMove( 1, 'O' )
-b.addMove( 1, 'X' )
-b.addMove( 2, 'O' )
-b.addMove( 2, 'X' )
-b.addMove( 2, 'X' )
-b.addMove( 3, 'X' )
-b.addMove( 3, 'O' )
-b.addMove( 3, 'X' )
-print b
-if b.winsFor('X'):
-    print '** INCORRECT **  b does not win for X'
-else:
-    print 'correct... b does not win for X'
-
-
-b.addMove( 3, 'X' )
-print b
-if b.winsFor('X'):
-    print 'correct... b does win for X'
-else:
-    print '** INCORRECT **  b does win for X'
-
-
-
-# test 8 - wins for diagonal (SE)
-print "\n\nTEST 8"
-b = Board(7,6)
-b.addMove( 6, 'X' )
-b.addMove( 5, 'O' )
-b.addMove( 5, 'X' )
-b.addMove( 4, 'O' )
-b.addMove( 4, 'X' )
-b.addMove( 4, 'X' )
-b.addMove( 3, 'X' )
-b.addMove( 3, 'O' )
-b.addMove( 3, 'X' )
-print b
-if b.winsFor('X'):
-    print '** INCORRECT **  b does not win for X'
-else:
-    print 'correct... b does not win for X'
-
-
-b.addMove( 3, 'X' )
-print b
-if b.winsFor('X'):
-    print 'correct... b does win for X'
-else:
-    print '** INCORRECT **  b does win for X'
-
-
+if __name__ == '__main__':
+    unittest.main()

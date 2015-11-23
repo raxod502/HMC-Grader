dec13a7786f68342b1988160b3425b4d3668e37e
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2015-11-23 15:48:22 -0800

    CS5G: Added tests for hw6, hw9, hw10 and hw11

--- TestFiles/CS5GTests/hw11tests/hw11pr2tests.py
+++ TestFiles/CS5GTests/hw11tests/hw11pr2tests.py
@@ -1,13 +1,6 @@
+import hw11pr2 as hw
 
-#
-# Connect 4 player grading script
-#
-
-
-import sys
-import traceback
-import hw11pr2; reload(hw11pr2) ; from hw11pr2 import *
-
+import unittest
 
 def setupBoard( b, s ):
   """ make moves from s into b """
@@ -19,309 +12,84 @@ def setupBoard( b, s ):
     else: ox = 'X'
   return b
 
-
-print
-print
-print ":-)   CS 5 graders rock!"
-print
-print
-    
-global TOTAL_SCORE
-TOTAL_SCORE = 0
-
-
-try:
-  print "\n\n+++ TEST 1 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  p = Player( 'X', 'LEFT', 0 )
-  p2 = Player( 'O', 'RIGHT', 1 )
-  # test 1 - does oppCh work?
-  print "p is\n", p, "\n"
-  if p.oppCh() == 'O':
-    print "  ",
-    print 'correct... p\'s opponent is \'O\''
-    TOTAL_SCORE += 3
-  else:
-    print '** INCORRECT **  p\'s opponent is not \'O\''
-    print
-    print "The return value was", p.oppCh()
-
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-
-
-
-try:
-  print "\n\n+++ TEST 2 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  p = Player( 'X', 'LEFT', 0 )
-  p2 = Player( 'O', 'RIGHT', 1 )
-  b.addMove( 0, 'X' )
-  b.addMove( 0, 'X' )
-  b.addMove( 0, 'X' )
-  print "b is\n", b, "\n"
-  if p.scoreBoard(b) == 50.0:
-    print "  ",
-    print 'correct... b scores at 50.0 for X'
-    TOTAL_SCORE += 5
-  else:
-    print '** INCORRECT **  b should score a 50.0 for X'
-    print
-    print "The return value was", p.scoreBoard(b)
-
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-
-
-try:
-  print "\n\n+++ TEST 3 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  p = Player( 'X', 'LEFT', 0 )
-  p2 = Player( 'O', 'RIGHT', 1 )
-  b.addMove( 0, 'X' )
-  b.addMove( 0, 'X' )
-  b.addMove( 0, 'X' )
-  b.addMove( 0, 'X' )
-  print "b is\n", b, "\n"
-  if p.scoreBoard(b) == 100.0:
-      print "  ",
-      print 'correct... b scores a 100.0 for X'
-      TOTAL_SCORE += 7
-  else:
-      print '** INCORRECT **  b should score a 100.0 for X'
-      print
-      print "The return value was", p.scoreBoard(b)
-      print
-  if p2.scoreBoard(b) == 0.0:
-      print "  ",
-      print 'correct... b scores a 0.0 for O'
-      TOTAL_SCORE += 4
-  else:
-      print '** INCORRECT **  b should score at 0.0'
-      print
-      print "The return value was", p2.scoreBoard(b)
-      print
-
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-
-
-
-
-
-try:
-  print "\n\n+++ TEST 4 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  p = Player( 'X', 'LEFT', 0 )
-  p2 = Player( 'O', 'RIGHT', 1 )
-  #print "b is\n", b
-  # does tie breaking work to the left and right... ?
-  scores = [ 0,100,0,0,0,100,0 ]
-  print "scores are", scores, "\n" 
-  if p.tiebreakMove( scores ) == 1:
-      print 'correct... p tiebreaks in column 1'
-      TOTAL_SCORE += 4
-  else:
-      print '** INCORRECT **  ... p should tiebreak in column 1'
-      print
-      print "The return value was", p.tiebreakMove( scores )
-      print
+class ConnectFourTests(unittest.TestCase):
+  def testOppCh(self):
+    b = hw.Board(7,6)
+    p = hw.Player( 'X', 'LEFT', 0 )
+    p2 = hw.Player( 'O', 'RIGHT', 1 )
+    self.assertEqual(p.oppCh(),'O', '** INCORRECT **  p\'s opponent is not \'O\'')
   
-  
-  if p2.tiebreakMove( scores ) == 5:
-      print 'correct... p2 tiebreaks in column 5'
-      TOTAL_SCORE += 4
-  else:
-      print '** INCORRECT **  ... p2 should tiebreak in column 5'
-      print
-      print "The return value was", p2.tiebreakMove( scores )
-      print
-  
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
+  def testScoreBoard1(self):
+    b = hw.Board(7,6)
+    p = hw.Player( 'X', 'LEFT', 0 )
+    p2 = hw.Player( 'O', 'RIGHT', 1 )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    self.assertEqual(p.scoreBoard(b), 50.0,'** INCORRECT **  b should score a 50.0 for X')
+  def testScoreBoard2(self):
+    b = hw.Board(7,6)
+    p = hw.Player( 'X', 'LEFT', 0 )
+    p2 = hw.Player( 'O', 'RIGHT', 1 )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    self.assertEqual(p.scoreBoard(b), 100.0,'** INCORRECT **  b should score a 100.0 for X')
+  def testScoreBoard3(self):
+    b = hw.Board(7,6)
+    p = hw.Player( 'X', 'LEFT', 0 )
+    p2 = hw.Player( 'O', 'RIGHT', 1 )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    b.addMove( 0, 'X' )
+    self.assertEqual(p2.scoreBoard(b), 0.0,'** INCORRECT **  b should score 0.0 for O')
 
+  def testTieBreakMove1(self):
+    b = hw.Board(7,6)
+    p = hw.Player( 'X', 'LEFT', 0 )
+    p2 = hw.Player( 'O', 'RIGHT', 1 )
+    scores = [ 0,100,0,0,0,100,0 ]
+    self.assertEqual(p.tiebreakMove( scores ), 1,'** INCORRECT ** p should tiebreak in column 1')
+  def testTieBreakMove2(self):
+    b = hw.Board(7,6)
+    p = hw.Player( 'X', 'LEFT', 0 )
+    p2 = hw.Player( 'O', 'RIGHT', 1 )
+    scores = [ 0,100,0,0,0,100,0 ]
+    self.assertEqual(p2.tiebreakMove( scores ), 5,'** INCORRECT ** p2 should tiebreak in column 5')
 
+  def testScoresFor1(self):
+    b = hw.Board(7,6)
+    b = setupBoard( b, '1211244445' )
+    answer = hw.Player('X', 'LEFT', 0).scoresFor(b)
+    self.assertEqual(answer, [50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0],'** INCORRECT ** 0 ply scoring test')
+  def testScoresFor2(self):
+    b = hw.Board(7,6)
+    b = setupBoard( b, '1211244445' )
+    answer = hw.Player('O', 'LEFT', 1).scoresFor(b)
+    self.assertEqual(answer, [50.0, 50.0, 50.0, 100.0, 50.0, 50.0, 50.0],'** INCORRECT ** 1 ply scoring test')
+  def testScoresFor3(self):
+    b = hw.Board(7,6)
+    b = setupBoard( b, '1211244445' )
+    answer = hw.Player('X', 'LEFT', 2).scoresFor(b)
+    self.assertEqual(answer, [0.0, 0.0, 0.0, 50.0, 0.0, 0.0, 0.0],'** INCORRECT ** 2 ply scoring test')
+  def testScoresFor4(self):
+    b = hw.Board(7,6)
+    b = setupBoard( b, '1211244445' )
+    answer = hw.Player('X', 'LEFT', 3).scoresFor(b)
+    self.assertEqual(answer, [0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0],'** INCORRECT ** 3 ply scoring test')
+  def testScoresFor5(self):
+    b = hw.Board(7,6)
+    b = setupBoard( b, '1211244445' )
+    answer = hw.Player('O', 'LEFT', 4).scoresFor(b)
+    self.assertEqual(answer, [0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0],'** INCORRECT ** 4 ply scoring test')
 
+# print "Be sure to run the two playGame tests...\n\n"
 
-try:
-  print "\n\n+++ TEST 5 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  b = setupBoard( b, '1211244445' )
-  answer = Player('X', 'LEFT', 0).scoresFor(b)
-  print "board is\n", b, "\n"
-  print "Running:  Player('X', 'LEFT', 0).scoresFor(b)\n"
-  if answer == [50.0, 50.0, 50.0, 50.0, 50.0, 50.0, 50.0]:
-      print 'correct... 0 ply scoring test'
-      TOTAL_SCORE += 4
-  else:
-      print '** INCORRECT **  ... 0 ply scoring test'
-      print
-      print "The return value was", answer
-      print
-  
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
+# print "Here they are for pasting, if you'd like:\n"
+# print "b = Board(7,6); b.playGame( Player('X','LEFT',0), Player('O','LEFT',0)) "
+# print "b = Board(7,6); b.playGame( Player('X','LEFT',1), Player('O','LEFT',1)) "
 
-
-
-
-try:
-  print "\n\n+++ TEST 6 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  b = setupBoard( b, '1211244445' )
-  answer = Player('X', 'LEFT', 0).scoresFor(b)
-  print "board is\n", b, "\n"
-  print "Running:  Player('O', 'LEFT', 1).scoresFor(b)\n"
-  answer = Player('O', 'LEFT', 1).scoresFor(b)
-  if answer == [50.0, 50.0, 50.0, 100.0, 50.0, 50.0, 50.0]:
-      print 'correct... 1 ply scoring test'
-      TOTAL_SCORE += 4
-  else:
-      print '** INCORRECT **  ... 1 ply scoring test'
-      print
-      print "The return value was", answer
-      print
-  
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-  
-
-
-try:
-  print "\n\n+++ TEST 7 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  b = setupBoard( b, '1211244445' )
-  answer = Player('X', 'LEFT', 0).scoresFor(b)
-  print "board is\n", b, "\n"
-  print "Running:  Player('X', 'LEFT', 2).scoresFor(b)\n"
-  answer = Player('X', 'LEFT', 2).scoresFor(b)
-  if answer == [0.0, 0.0, 0.0, 50.0, 0.0, 0.0, 0.0]:
-      print 'correct... 2 ply scoring test'
-      TOTAL_SCORE += 6
-  else:
-      print '** INCORRECT **  ... 2 ply scoring test'
-      print
-      print "The return value was", answer
-      print
-  
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-
-
-
-
-
-try:
-  print "\n\n+++ TEST 8 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  b = setupBoard( b, '1211244445' )
-  answer = Player('X', 'LEFT', 0).scoresFor(b)
-  print "board is\n", b, "\n"
-  print "Running:  Player('X', 'LEFT', 3).scoresFor(b)\n"
-  answer = Player('X', 'LEFT', 3).scoresFor(b)
-  if answer == [0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0]:
-      print 'correct... 3 ply scoring test'
-      TOTAL_SCORE += 6
-  else:
-      print '** INCORRECT **  ... 3 ply scoring test'
-      print
-      print "The return value was", answer
-      print
-  
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-
-
-try:
-  print "\n\n+++ TEST 9 +++\n"
-  b = Board(7,6)
-  hw11pr2.b = b
-  b = setupBoard( b, '1211244445' )
-  answer = Player('X', 'LEFT', 0).scoresFor(b)
-  print "board is\n", b, "\n"
-  print "Running:  Player('O', 'LEFT', 4).scoresFor(b)\n"
-  answer = Player('O', 'LEFT', 4).scoresFor(b)
-  if answer == [0.0, 0.0, 0.0, 100.0, 0.0, 0.0, 0.0]:
-      print 'correct... 4 ply scoring test'
-      TOTAL_SCORE += 8
-  else:
-      print '** INCORRECT **  ... 4 ply scoring test'
-      print
-      print "The return value was", answer
-      print
-
-except Exception as e:
-  print "** INCORRECT **   this test failed with an Exception:\n"
-  type_, value_, traceback_ = sys.exc_info()
-  tb = traceback.format_exc()
-  print tb
-  print
-
-
-
-
-
-
-
-# total score
-print "\n\nTOTAL POINTS EARNED (out of 55): ", TOTAL_SCORE, "\n\n"
-
-print "Be sure to run the two playGame tests...\n\n"
-
-print "Here they are for pasting, if you'd like:\n"
-print "b = Board(7,6); b.playGame( Player('X','LEFT',0), Player('O','LEFT',0)) "
-print "b = Board(7,6); b.playGame( Player('X','LEFT',1), Player('O','LEFT',1)) "
-
-
-
-print "\n\n"
-
-
-
-# let's see if this works...
+if __name__ == '__main__':
+    unittest.main()

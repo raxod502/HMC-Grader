477904e5ceb358453d8e096ff7b05c590ac355cb
Noelle Fa-Kaji <noelle.fakaji@yahoo.com>
2016-02-11 15:28:26 -0800

    Appearance: First trial of student viewEuros page
    This first pass has the euros displayed in a horizontal table (green boxes
    indicate a submission and no euro used, red checks indicate a euro was used
    because that submission was late). Euros are counted as being for a whole
    assignment (so multiple late parts in one assignment are okay). This view
    is an idea of how an instructor viewEuros page might look -- not meant to
    be final for the student view. Improvements include: making the table
    vertical, and possibly not breaking the detail down into individual problems

--- app/plugins/latework/euros.py
+++ app/plugins/latework/euros.py
@@ -7,24 +7,24 @@ def calculateGrades(gradeList):
   Basic (and default) late calculator only colors late submissions red
   so that they can be noticed in the gradebook
   '''
-  lateAssignments = []
+  lateProblems = []
   for i, assignment in enumerate(gradeList):
     for j, problem in enumerate(assignment):
       #check for no submission and skip
       if problem is None:
         continue
       if problem['isLate']:
-        lateAssignments.append((i,j))
+        lateProblems.append((i,j))
 
-  lateAssignments.sort(reverse=True, key=lambda x: gradeList[x[0]][x[1]]['rawTotalScore'])
+  lateProblems.sort(reverse=True, key=lambda x: gradeList[x[0]][x[1]]['rawTotalScore'])
   num = 0
-  while num < 3 and num < len(lateAssignments):
-    pos = lateAssignments[num]
+  while num <= 3 and num < len(lateProblems):
+    pos = lateProblems[num]
     gradeList[pos[0]][pos[1]]['highlight'] = "yellow"
     num += 1
-  while num < len(lateAssignments):
-    pos = lateAssignments[num]
+  while num < len(lateProblems):
+    pos = lateProblems[num]
     gradeList[pos[0]][pos[1]]['highlight'] = "red"
-    gradeList[pos[0]][pos[1]]['finalTotalScore'] = Decimal('0.00')
+    # gradeList[pos[0]][pos[1]]['finalTotalScore'] = Decimal('0.00')
     num += 1
   return gradeList

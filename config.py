89c333c4f290e01e18d740a167102a07e4ccc8cb
Bryan Mehall <bryanmehall@gmail.com>
2016-08-10 17:11:02 -0700

    use netifaces for IP

--- config.py
+++ config.py
@@ -6,17 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 import socket
 import fcntl
 import struct
+import netifaces as ni
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
 
-SERVER_IP = get_ip_address('eth0')  # '192.168.0.110'
+ni.ifaddresses('eth0')
+SERVER_IP = ni.ifaddresses('eth0')[2][0]['addr']
 print 'running as ', SERVER_IP
+
 #
 # Settings for WTForms
 #
